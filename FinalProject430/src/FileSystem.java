a4887189a9ae6b3b990ccdc5e064fa48297f79eb
rattanakchea <rattanak.chea@gmail.com>
2014-06-03 18:29:47 -0700

    file system

--- FinalProject430/src/FileSystem.java
+++ FinalProject430/src/FileSystem.java
@@ -4,11 +4,34 @@
  */
 public class FileSystem {
 	private Superblock superblock;
-	pirvate Directory directory;
-	private FileStructureTable filetable;
-	
-	public FileSystem(int diskBlocks){
+	private Directory directory;
+	private FileTableEntry filetable;
+
+	public FileSystem(int diskBlocks) {
 		superblock = new SuperBlock(diskBlocks);
 		directory = new Directory(superblock.totalInodes);
+		filetable = new FileTableEntry(directory);
+
+		// read the "/" file from disk
+		FileTableEntry dirEnt = open("/", "r");
+		int dirSize = fsize(dirEnt);
+		if (dirSize > 0) {
+			// directory has some data
+			byte[] dirData = new byte[dirSize];
+			read(dirEnt, dirData);
+			directory.byte2directory(dirData);
+		}
+		close(dirEnt);
 	}
+
+	// methods
+	public void close(FileTableEntry dirEnt) {
+
+	}
+
+	// methods
+	public void read(FileTableEntry dirEnt, byte[] data) {
+
+	}
+
 }
