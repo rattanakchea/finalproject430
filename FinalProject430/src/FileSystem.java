1a85233ba233568b848e987e21cdcf1c91c59366
taloto07 <taloto07@gmail.com>
2014-06-11 15:40:11 -0700

    latest FileSystem.java

--- FinalProject430/src/FileSystem.java
+++ FinalProject430/src/FileSystem.java
@@ -5,23 +5,27 @@
 public class FileSystem {
 	private SuperBlock superblock;
 	private Directory directory;
-	private FileTableEntry filetable;
+	private FileTable filetable;
+	
+	public static final int SEEK_SET = 0;
+	public static final int SEEK_CUR = 1;
+	public static final int SEEK_END = 2;
 
 	public FileSystem(int diskBlocks) {
 		superblock = new SuperBlock(diskBlocks);
-//		directory = new Directory(superblock.totalInodes);
-//		filetable = new FileTableEntry(directory);
-//
-//		// read the "/" file from disk
-		//FileTableEntry dirEnt = this.open("/", "r");
-//		int dirSize = fsize(dirEnt);
-//		if (dirSize > 0) {
-//			// directory has some data
-//			byte[] dirData = new byte[dirSize];
-//			read(dirEnt, dirData);
-//			directory.byte2directory(dirData);
-//		}
-//		this.close(dirEnt);
+		directory = new Directory(superblock.totalInodes);
+		filetable = new FileTable(directory);
+
+		// read the "/" file from disk
+		FileTableEntry dirEnt = this.open("/", "r");
+		int dirSize = fsize(dirEnt);
+		if (dirSize > 0) {
+			// directory has some data
+			byte[] dirData = new byte[dirSize];
+			read(dirEnt, dirData);
+			directory.byte2directory(dirData);
+		}
+		this.close(dirEnt);
 	}
 
 	public boolean format(int paramInt) {
@@ -34,6 +38,11 @@ public class FileSystem {
 //		this.filetable = new FileTable(this.directory);
 
 		return true;
+	}
+	
+	public FileTableEntry open(String fileName, String mode) throws InterruptedException{
+		FileTableEntry localFileTableEntry = filetable.falloc(fileName, mode);
+		return null;
 	}
 
 	// methods
@@ -48,7 +57,10 @@ public class FileSystem {
 
 	// methods
 	public int fsize(FileTableEntry dirEnt) {
-		return 1;
+		if (dirEnt == null)
+			return -1;
+		
+		return dirEnt.inode.length;
 	}
 
 	// methods
